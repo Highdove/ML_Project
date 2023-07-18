cd7c22d0757c78a00cb4f76772ce2fbee241e00d
Highdove <junoton@gmail.com>
2023-07-18 16:42:31 +0100

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 from src.logger import logging
 import pandas as pd
 
@@ -46,4 +48,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiat_data_Ingestion()
+    train_data, test_data = obj.initiat_data_Ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
